7b6f2d14c546d4f1ff6c900b6224d6289ac3b323
Msksgm <msksgm3124@gmail.com>
2023-01-05 15:15:08 +0900

    build: arrow を導入
    - dependencies に arrow を記述

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,6 +70,22 @@ dependencies {
      * dokka Pluginを適用するのに必要
      */
     dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.7.20")
+
+    /**
+     * Arrow Core
+     *
+     * URL
+     * - https://arrow-kt.io/docs/core/
+     * MavenCentral
+     * - https://mvnrepository.com/artifact/io.arrow-kt/arrow-core
+     * Main用途
+     * - Either/Validatedを使ったRailway Oriented Programming
+     * Sub用途
+     * - Optionを使ったletの代替
+     * 概要
+     * - Kotlinで関数型プログラミングをするときに便利なライブラリ
+     */
+    implementation("io.arrow-kt:arrow-core:1.1.3")
 }
 
 tasks.withType<KotlinCompile> {
