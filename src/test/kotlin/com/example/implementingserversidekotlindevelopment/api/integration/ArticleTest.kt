27202d0384c4671af5affa4996ba25c41ae859b9
Msksgm <msksgm3124@gmail.com>
2023-01-27 07:05:52 +0900

    test: get /api/articles/:slug の準正常系「slug のフォーマットが不正な場合、バリデーションエラー」を実装

--- src/test/kotlin/com/example/implementingserversidekotlindevelopment/api/integration/ArticleTest.kt
+++ src/test/kotlin/com/example/implementingserversidekotlindevelopment/api/integration/ArticleTest.kt
@@ -72,5 +72,44 @@ class ArticleTest {
                 JSONCompareMode.STRICT
             )
         }
+
+        @Test
+        fun `準正常系-slug のフォーマットが不正な場合、バリデーションエラー`() {
+            /**
+             * given:
+             */
+            val slug = "dummy-slug"
+
+            /**
+             * when:
+             */
+            val response = mockMvc.get("/api/articles/$slug") {
+                contentType = MediaType.APPLICATION_JSON
+            }.andReturn().response
+            val actualStatus = response.status
+            val actualResponseBody = response.contentAsString
+
+            /**
+             * then:
+             * - ステータスコードが一致する
+             * - レスポンスボディが一致する
+             */
+            val expectedStatus = HttpStatus.FORBIDDEN.value()
+            val expectedResponseBody = """
+                {
+                  "errors": {
+                    "body": [
+                      "slug は 32 文字の英小文字数字です。"
+                    ]
+                  }
+                }
+            """.trimIndent()
+            assertThat(actualStatus).isEqualTo(expectedStatus)
+            JSONAssert.assertEquals(
+                expectedResponseBody,
+                actualResponseBody,
+                JSONCompareMode.NON_EXTENSIBLE
+            )
+        }
     }
 }
