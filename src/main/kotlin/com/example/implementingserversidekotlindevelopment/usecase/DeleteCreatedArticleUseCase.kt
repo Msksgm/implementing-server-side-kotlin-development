5b30539049b375d71d9a301c88975eb3b4787247
Msksgm <msksgm3124@gmail.com>
2023-03-01 07:53:17 +0900

    feat: DeleteCreatedArticleUseCaseImpl と ArticleRepository.delete の正常系を実装

--- src/main/kotlin/com/example/implementingserversidekotlindevelopment/usecase/DeleteCreatedArticleUseCase.kt
+++ src/main/kotlin/com/example/implementingserversidekotlindevelopment/usecase/DeleteCreatedArticleUseCase.kt
@@ -1,6 +1,9 @@
 package com.example.implementingserversidekotlindevelopment.usecase
 
 import arrow.core.Either
+import arrow.core.handleError
+import arrow.core.right
+import com.example.implementingserversidekotlindevelopment.domain.ArticleRepository
 import com.example.implementingserversidekotlindevelopment.domain.Slug
 import com.example.implementingserversidekotlindevelopment.util.ValidationError
 import org.springframework.stereotype.Service
@@ -42,6 +45,25 @@ interface DeleteCreatedArticleUseCase {
 /**
  * 記事削除ユースケースの具象クラス
  *
+ * @property articleRepository
  */
 @Service
-class DeleteCreatedArticleUseCaseImpl : DeleteCreatedArticleUseCase
+class DeleteCreatedArticleUseCaseImpl(
+    val articleRepository: ArticleRepository,
+) : DeleteCreatedArticleUseCase {
+    override fun execute(slug: String?): Either<DeleteCreatedArticleUseCase.Error, Unit> {
+        val validatedSlug = Slug.new(slug = slug).fold(
+            { TODO() },
+            { it }
+        )
+
+        articleRepository.delete(validatedSlug)
+            .handleError {
+                when (it) {
+                    is ArticleRepository.DeleteError.NotFound -> TODO()
+                }
+            }
+
+        return Unit.right()
+    }
+}
