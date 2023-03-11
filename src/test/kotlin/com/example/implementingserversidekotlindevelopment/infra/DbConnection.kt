b266aade58039e376fdf2393a063b842fc0d9b4a
Msksgm <msksgm3124@gmail.com>
2023-03-12 07:00:52 +0900

    docs: DbConnection にコードコメントを記述

--- src/test/kotlin/com/example/implementingserversidekotlindevelopment/infra/DbConnection.kt
+++ src/test/kotlin/com/example/implementingserversidekotlindevelopment/infra/DbConnection.kt
@@ -6,7 +6,19 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import javax.sql.DataSource
 
+/**
+ * Db connection
+ *
+ * DB 統合テストで DB 設定をおこなうメソッド
+ *
+ * @constructor Create empty Db connection
+ */
 object DbConnection {
+    /**
+     * テスト時に DB 設定をするメソッド
+     *
+     * @return
+     */
     fun dataSource(): DataSource {
         val hikariConfig = HikariConfig()
         hikariConfig.jdbcUrl = "jdbc:postgresql://127.0.0.1:5432/sample-db"
@@ -22,6 +34,12 @@ object DbConnection {
 
     val namedParameterJdbcTemplate = NamedParameterJdbcTemplate(dataSource())
 
+    /**
+     * テスト時にテーブルのシーケンスを設定するメソッド
+     *
+     * 全てのテスト実行前に呼び出すことで、該当カラムの初期値が固定される
+     *
+     */
     fun resetSequence() {
         val sequenceValue = 10000
         val sql = """
